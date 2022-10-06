26fc78fd5e3ae0edb734a535316960b082b5cd27
“misskukuua” <p.asare@alustudent.com>
2022-10-06 07:17:33 -0700

    task

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const myFunction = process.argv;
-console.log(myFunction === 2 ? 'No argument' : myFunction === 3 ? 'Argument found' : 'Arguments found');
+console.log(myFunction === 2 ? 'Argument found' : myFunction === 3 ? 'Arguments found' : 'Argument found');
