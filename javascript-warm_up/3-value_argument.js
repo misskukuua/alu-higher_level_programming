d10dce6bf6aa1192219c62f1a8a40836d5511453
“misskukuua” <p.asare@alustudent.com>
2022-10-06 01:42:01 -0700

    value

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 const myFunction = process.argv;
-if (myFunction !== 1) {
-	console.log('No argument')
+if (myFunction > 2) {
+  console.log('HBTN cool')
+} else if (myFunction === 1) {
+  console.log('HBTN')
+} else {
+  console.log('No argument');
 }
