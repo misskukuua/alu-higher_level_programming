784903a5029b5193b9046fd038064057392181f6
“misskukuua” <p.asare@alustudent.com>
2022-10-06 01:06:18 -0700

    all

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+function myFunction ()
+if (myFunction.length === 4) {
+	console.log('Argument found');
+}
+else if (myFunction > 4) {
+        console.log('Arguments found');
+}
+else {
+        console.log('No argument');
+}
+
