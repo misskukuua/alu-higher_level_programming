6165c4d645f9e4185f5f1f43e5b3f6fc3a3df336
“misskukuua” <p.asare@alustudent.com>
2022-10-10 11:49:42 -0700

    all

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-const myFunction = (process.argv[2]);
-
-if (myFunction) {
-  console.log('My number: ' + myFunction);
+const myFunction = process.argv;
+const conV = Number(myFunction[2]);
+if (conV) {
+  console.log(`My number: ${myFunction[2]}`);
 } else {
-  console.log('Not a number);
+  console.log('Not a number');
+}
