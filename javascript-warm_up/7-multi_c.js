e685ad5d3cc426bd7bfee3ede900dbf4e2f8afc6
“misskukuua” <p.asare@alustudent.com>
2022-10-10 14:46:49 -0700

    all

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -3,8 +3,8 @@ const myFunction = process.argv;
 const youFunction = Number(myFunction[2]);
 if (youFunction) {
   for (let i = 0; i < youFunction; i++) {
-    console.log('C is fun')
-  } 
+    console.log('C is fun');
+  }
 } else {
-   console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 }
